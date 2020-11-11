0bc7c438d2b1a0bb2b49529bf558995d88421130
Phan Duc Hieu <Phan Duc Hieu@DESKTOP-S6M2QEJ>
2020-11-11 22:13:17 +0700

    Generate variable, constructor, getter and setter in contest class

--- src/assignment/Contest.java
+++ src/assignment/Contest.java
@@ -10,5 +10,51 @@ package assignment;
  * @author Phan Duc Hieu
  */
 public class Contest {
+    public String contestId;
+    public String Date;
+    public String Author;
+    public String QuestionId;
+
+    public String getContestId() {
+        return contestId;
+    }
+
+    public void setContestId(String contestId) {
+        this.contestId = contestId;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String Author) {
+        this.Author = Author;
+    }
+
+    public String getQuestionId() {
+        return QuestionId;
+    }
+
+    public void setQuestionId(String QuestionId) {
+        this.QuestionId = QuestionId;
+    }
+    
+    public Contest() {
+    }
+    
+    public Contest(String contestId, String Date, String Author, String QuestionId) {
+        this.contestId = contestId;
+        this.Date = Date;
+        this.Author = Author;
+        this.QuestionId = QuestionId;
+    }
     
 }
